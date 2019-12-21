9b5af60b0b6d0cbfd7766dc45ae197fa0c1c7af3
Alex Bannow <rbannal@gmail.com>
2019-12-21 13:21:51 -0500

    cors

--- Petful-server/src/server.js
+++ Petful-server/src/server.js
@@ -18,9 +18,7 @@ const morganOption = (NODE_ENV === 'production')
 
 
 const app = express();
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 app.use(morgan(morganOption));
 
 app.use('/api/dogs', dogRouter);
